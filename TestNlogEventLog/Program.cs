1fb7b228bb00b8798923b747648f7fc08ec414ad
WorldCount <World.Count@yandex.ru>
2022-02-22 15:56:59 +0300

    Добавьте файлы проекта.

---
+++ TestNlogEventLog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NLog;
+using NLog.Targets;
+
+namespace TestNlogEventLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NLog.LogManager.GetLogger("ApplicationEventLog").Error("Kilroy was here");
+        }
+    }
+}
